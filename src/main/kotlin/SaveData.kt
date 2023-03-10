1ed22cb35a6d30114d710bf922b8a48ad9b3746b
Stefan Gerstner <stefan.gerstner96@web.de>
2023-03-10 17:57:38 +0100

    Cleaned up Main & added Save Data Functions

--- src/main/kotlin/SaveData.kt
+++ src/main/kotlin/SaveData.kt
@@ -1,1 +1,27 @@
-data class SaveData(val recipes: List<Recipe>)
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.io.File
+
+data class SaveData(val recipes: MutableList<Recipe>)
+
+fun readSaveData(path: String = "./src/main/resources/SaveFile.json"):SaveData
+{
+    val jsonString: String = File(path).readText(Charsets.UTF_8)
+    return Gson().fromJson(jsonString, SaveData::class.java)
+}
+
+fun writeSaveData(
+    data: SaveData,
+    path: String = "./src/main/resources/SaveFile.json"
+)
+{
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    val jsonString = gson.toJson(data)
+    File(path).writeText(jsonString,Charsets.UTF_8)
+}
+fun addRecipeToSaveData(recipe: Recipe)
+{
+    val data = readSaveData()
+    data.recipes.add(recipe)
+    writeSaveData(data)
+}
